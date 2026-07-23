86df4186fdd01da0b715a0f7701a19b07f10cd33
Lakshmi Malleswari Komara <komaramalleswari123@gmail.com>
2026-07-23 22:45:02 +0530

    Fix Order model import path

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,4 @@
-const Order = require("../models/Order");
+const Order = require("../models/order");
 const Food = require("../models/Food");
 const mongoose = require("mongoose");
 // Customer: Place new order
